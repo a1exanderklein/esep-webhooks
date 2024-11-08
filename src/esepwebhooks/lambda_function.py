59165d0cf2b403001a10a73b1798a6e8326bb64a
Alexander Klein <agklein04@outlook.com>
2024-11-07 19:41:09 -0500

    Fix 2

--- src/esepwebhooks/lambda_function.py
+++ src/esepwebhooks/lambda_function.py
@@ -2,15 +2,13 @@ import os
 import json
 import requests
 
-def function_handler(input):
-    data = json.loads(input)
+def function_handler(event):
+    issueURL = event.get("issue", {}).get("html_url", "No URL")
 
-    payload = {
-        "text": f"Issue Created: {data['issue']['html_url']}"
-    }
+    payload = json.dumps({"text": f"Issue Created: {issueURL}"})
 
     slack_url = os.getenv("SLACK_URL")
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(slack_url, headers=headers, data=json.dumps(payload))
+    response = requests.post(slack_url, headers=headers, data=payload)
 
     return response.text
